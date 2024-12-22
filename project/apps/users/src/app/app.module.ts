932791cde0e243d2329abaac62b477e4fbadb2be
George G <grreulvi@gmail.com>
2024-12-23 02:05:53 +0300

    [module2-task1] wip

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogUserModule } from '@project/blog-user';
+import { AuthenticationModule } from '@project/authentication';
 
 @Module({
-  imports: [],
+  imports: [
+    BlogUserModule,
+    AuthenticationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
