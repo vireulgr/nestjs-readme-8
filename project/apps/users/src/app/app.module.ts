09014e44dacf465347138932585cdc423c10218a
vireulgr <grreulvi@gmail.com>
2025-01-27 03:26:35 +0300

    [module3-task1] add user config module import and getting of port config value in bootstrap

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from '@project/authentication';
 import { BlogUserModule } from '@project/blog-user';
+import { UserConfigModule } from '@project/user-config';
 
 @Module({
   imports: [
     AuthenticationModule,
+    UserConfigModule,
     BlogUserModule,
   ],
 })
