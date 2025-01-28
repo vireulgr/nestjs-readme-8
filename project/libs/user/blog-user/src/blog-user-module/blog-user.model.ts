870dfa361ebf20c2bf12ea461d30f28bc5c55a66
Георгий Грищенко <ggv@confident.com>
2025-01-28 18:04:02 +0300

    [module3-task1] add user mongo model

---
+++ project/libs/user/blog-user/src/blog-user-module/blog-user.model.ts
@@ -0,0 +1,49 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AuthUser, UserRole } from '@project/core';
+
+@Schema({
+  collection: 'users',
+  timestamps: true,
+})
+export class BlogUserModel extends Document implements AuthUser {
+  @Prop()
+  public avatar: string;
+
+  @Prop({
+    required: true,
+  })
+  public dateOfBirth: Date;
+
+  @Prop({
+    required: true,
+    unique: true
+  })
+  public email: string;
+
+  @Prop({
+    required: true,
+  })
+  public firstName: string;
+
+  @Prop({
+    required: true,
+  })
+  public lastName: string;
+
+
+  @Prop({
+    required: true,
+  })
+  public passwordHash: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRole,
+    default: UserRole.User,
+  })
+  public role: UserRole;
+}
+
+export const BlogUserSchema = SchemaFactory.createForClass(BlogUserModel);
